0f609cf1e95c0c0332e18764960931457efc27c0
GautamSood <gautamsood.cse19@chitkarauniversity.edu.in>
2023-10-19 00:01:55 +0530

    first commit

---
+++ src/main/java/com/fil/taptocure/TapToCureApplication.java
@@ -0,0 +1,13 @@
+package com.fil.taptocure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapToCureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapToCureApplication.class, args);
+    }
+
+}
